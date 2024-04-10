af49e541ce45a1e39b21194e711fbeef50b58f6f
Haoyu Sun <sunhaoyu468@gmail.com>
2024-04-10 23:50:32 +1000

    JB-3: Initial commit

---
+++ JobBot-server/jobbot/jobbot/src/main/java/org/dizzybot/jobbot/JobbotApplication.java
@@ -0,0 +1,13 @@
+package org.dizzybot.jobbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbotApplication.class, args);
+	}
+
+}
---
+++ JobBot-server/jobbot/jobbot/src/test/java/org/dizzybot/jobbot/JobbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dizzybot.jobbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
