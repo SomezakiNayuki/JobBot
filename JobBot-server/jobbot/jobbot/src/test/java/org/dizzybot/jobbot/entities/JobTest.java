5aa2df395866bf2dee7d6a17e46186cc358bc2e7
Fitz C <1343571774@qq.com>
2024-05-27 23:59:57 +1000

    JB-33 - JB-36 Basically completed the establishment of job, account, payment, outcome class with their unittests.
    Signed-off-by: Fitz C <1343571774@qq.com>

---
+++ JobBot-server/jobbot/jobbot/src/test/java/org/dizzybot/jobbot/entities/JobTest.java
@@ -0,0 +1,52 @@
+package org.dizzybot.jobbot.entities;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JobTest {
+
+    private User user;
+
+    private List<Job> jobAccepted;
+    private List<Job> jobPosted;
+
+    private Job jobTest;
+
+    private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @Before
+    public void beforeEach() {
+        this.user = new User("Dizzy Bot", "Password", "dizzybot@email.com");
+        this.jobAccepted = this.user.jobAccepted;
+        this.jobPosted = this.user.jobPosted;
+    }
+
+    @After
+    public void afterEach() {
+        this.jobAccepted = null;
+        this.jobPosted = null;
+    }
+
+    @Test
+    public void testUserJobListsCreateByUser() {
+        assertNotNull(this.user.jobAccepted);
+        assertNotNull(this.user.jobPosted);
+    }
+
+    @Test
+    public void testUserAccountCreateByUserViolation() {
+        assertEquals(0, this.user.getJobAccepted().size()); // []
+        assertEquals(0, this.user.getJobPosted().size()); // []
+        // todo test adding job
+    }
+
+}
