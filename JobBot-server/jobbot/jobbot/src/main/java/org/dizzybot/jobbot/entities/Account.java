fe748591b261a0b2b330f45b9f80ccbde583edb8
Fitz C <1343571774@qq.com>
2024-05-27 23:59:57 +1000

    JB-33 - JB-36 Basically completed the establishment of job, account, payment, outcome class with their unittests.

---
+++ JobBot-server/jobbot/jobbot/src/main/java/org/dizzybot/jobbot/entities/Account.java
@@ -0,0 +1,45 @@
+package org.dizzybot.jobbot.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+
+@Entity
+@Getter
+@Setter
+public class Account {
+
+    @NonNull
+    private long cardNumber; // should have some constrains
+
+    @Nullable
+    private long bsb;
+
+    @Id
+    private long accountNumber; // currently using accountNumber as primary key
+
+    @NonNull
+    private double accountBalance;
+
+    @OneToMany(mappedBy = "account", cascade = ALL)
+    private List<Payment> paymentHistory = new ArrayList<Payment>();
+
+    @OneToOne
+    @JoinColumn(name = "user_Account")
+    private User user;
+
+    public Account() {}
+
+    public Account(long cardNumber) {
+        this.cardNumber = cardNumber;
+        this.paymentHistory = new ArrayList<>();
+
+    }
+}
---
+++ JobBot-server/jobbot/jobbot/src/main/java/org/dizzybot/jobbot/enums/JobStatusEnum.java
@@ -0,0 +1,9 @@
+package org.dizzybot.jobbot.enums;
+
+public enum JobStatusEnum {
+    TO_POST,
+    POSTING,
+    EXPIRED,
+    IN_PROGRESS,
+    COMPLETED;
+}
