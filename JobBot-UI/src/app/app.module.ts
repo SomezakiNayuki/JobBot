4874835bd1997c3334a5e31802ced7cf8d31e73b
Haoyu Sun <2502651290@qq.com>
2024-06-12 02:00:20 +1000

    JB-21: Set up Angular router

--- JobBot-UI/src/app/app.module.ts
+++ JobBot-UI/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
