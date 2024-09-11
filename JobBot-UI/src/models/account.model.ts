8d45c61417a34666889ddf9c130e6685e39798c9
Haoyu Sun <2502651290@qq.com>
2024-09-11 20:58:09 +1000

    JB-42: Create JobBot UI model of Account

---
+++ JobBot-UI/src/models/account.model.ts
@@ -0,0 +1,14 @@
+// No unit test required for static model until business logic introduced.
+export default class Account {
+  public id: number;
+  public cardNumber: number;
+  public bsb: number;
+  public accountNumber: number;
+  public accountBalance: number;
+  public paymentHistory: Payment[];
+
+
+  public Account() {}
+
+  // No getter setter required needed, directly assign value through public attributes.
+}
