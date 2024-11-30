c44abd986702900549d77ca66b353f99c796e5fb
Haoyu Sun <2502651290@qq.com>
2024-11-30 20:18:57 +1100

    JB-60: Add UT fro job image API

--- JobBot-server/jobbot/jobbot/src/test/java/org/dizzybot/jobbot/controllers/JobControllerTests.java
+++ JobBot-server/jobbot/jobbot/src/test/java/org/dizzybot/jobbot/controllers/JobControllerTests.java
@@ -3,6 +3,8 @@ package org.dizzybot.jobbot.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dizzybot.jobbot.controllers.job.JobController;
 import org.dizzybot.jobbot.controllers.job.requests.CreateJobRequest;
+import org.dizzybot.jobbot.entities.Job;
+import org.dizzybot.jobbot.entities.JobImage;
 import org.dizzybot.jobbot.entities.User;
 import org.dizzybot.jobbot.services.JobService;
 import org.dizzybot.jobbot.services.UserService;
@@ -13,14 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(JobController.class)
@@ -68,6 +70,43 @@ public class JobControllerTests {
     @Test
     public void testGetJob() throws Exception {
         ResultActions resultActions = mockMvc.perform(get("/api/job/get"));
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void testUploadImageSuccess() throws Exception {
+        MockMultipartFile mockFile = new MockMultipartFile(
+                "file",
+                "image.jpeg",
+                "image/jpeg",
+                "1234567890".getBytes()
+        );
+
+        when(jobService.findById(Mockito.any())).thenReturn(new Job());
+
+        ResultActions resultActions = mockMvc.perform(multipart("/api/job/uploadImage/1")
+                .file(mockFile)
+                .contentType(MediaType.MULTIPART_FORM_DATA));
+
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().json("{\"status\":\"success\",\"message\":\"Job image uploaded\"}"));
+    }
+
+    @Test
+    public void testGetJobImages() throws Exception {
+        Job job = new Job();
+        JobImage image = new JobImage();
+        image.setId(1L);
+        image.setImage("12345".getBytes());
+        image.setJob(job);
+        job.getImages().add(image);
+
+        when(jobService.findById(Mockito.any())).thenReturn(job);
+
+        ResultActions resultActions = mockMvc.perform(get("/api/job/get/1/images"));
 
         resultActions.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
